9adfec172621c2654eb960dcb9735875fde06338
Sky Winston <skywinston@gmail.com>
2015-07-26 12:15:27 -0600

    Including main files

--- celsenheit.js
+++ celsenheit.js
@@ -1,5 +1,8 @@
 //var temp = prompt("What temperature in F would you like to convert to C?");
 
+//Keep focus in the temperature input field
+$('#temperature_input').on('blur',function (event) { var blurEl = $(this); setTimeout(function() {blurEl.focus()},10) });
+
 //Toggle between F to C conversion & vice-versa
 function conversionToggle() {
   if ( $('#conversion_submission').attr('onclick') == 'convertFtoC()' ) {
@@ -16,6 +19,7 @@ function convertFtoC() {
   var numberToConvert = document.getElementById("temperature_input").value;
   numberToConvert = Math.round( (5/9)*(numberToConvert-32) );
   document.getElementById("answer").innerHTML = "" + numberToConvert;
+  $( "#answer" ).append( "<span class='degrees'> &#176;C</span>" );
   if ( numberToConvert <= 0 ) {
     $('body').attr('class', 'really_cold'); //really cold
   } else if ( numberToConvert > 0 && numberToConvert <= 10) {
@@ -32,6 +36,7 @@ function convertCtoF() {
   var numberToConvert = document.getElementById("temperature_input").value;
   numberToConvert = Math.round( ((9/5)*numberToConvert)+32 );
   document.getElementById("answer").innerHTML = "" + numberToConvert;
+  $( "#answer" ).append( "<span class='degrees'> &#176;F</span>" );
   if ( numberToConvert <= 32 ) {
     $('body').attr('class', 'really_cold'); //really cold
   } else if ( numberToConvert > 32 && numberToConvert <= 50) {
@@ -44,12 +49,20 @@ function convertCtoF() {
 }
 
 //Keyboard Shortcut for Toggle
-window.addEventListener("keyup", shortCuts, false);
-function shortCuts(k){
+window.addEventListener("keyup", toggleShortCut, false);
+function toggleShortCut(k){
   if (k.keyCode == "84") {
     conversionToggle();
   }
 }
+
+window.addEventListener("keyup", submitShortCut, false);
+function submitShortCut(k) {
+  if (k.keyCode == "13") {
+    $("#conversion_submission").trigger("click");
+  }
+}
+
 
 
 //alert(convertFtoC(temp));
